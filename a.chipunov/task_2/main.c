c14446d4eb6562155ab61213f1bd6492adbc6f2f
OsipShmel <184374532+OsipShmel@users.noreply.github.com>
2026-09-15 19:39:31 +0700

    fix: task2

--- a.chipunov/task_2/main.c
+++ a.chipunov/task_2/main.c
@@ -8,7 +8,7 @@ int main()
 {
     time_t now;
     struct tm *sp;
-    (void) putenv("TZ=PST8PDT");
+    (void) putenv("TZ=PST8");
     (void) time( &now );
 
     tzset();
@@ -18,7 +18,7 @@ int main()
     sp = localtime(&now);
     printf("%d/%d/%02d %d:%02d %s\n",
         sp->tm_mon + 1, sp->tm_mday,
-        sp->tm_year, sp->tm_hour,
+        sp->tm_year+1900, sp->tm_hour,
         sp->tm_min, tzname[sp->tm_isdst]);
     exit(0);
 }
